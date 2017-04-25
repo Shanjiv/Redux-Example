2206eaf924f1a54c9c2805c70a261437073b311e
Shanseevan Kanagaratnam <shanjiv@hotmail.com>
2017-04-25 17:56:17 +0200

    Add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,10 +5,26 @@ console.log('Starting redux example');
 var reducer = (state = {name: 'Anonymous'}, action) => {
   //state = state || {name: 'Anonymous'};
 
-  return state;
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+    return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currenState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Andrew'
+});
+
+console.log('Name should be andrew', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,7 +8,15 @@ var stateDefault = {
   todos: []
 };
 var reducer = (state = stateDefault, action) => {
-
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+    default:
+      return state;
+  }
 
   return state;
 };
@@ -17,3 +25,10 @@ var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currenState', currentState);
+
+store.dispatch({
+  type:'CHANGE_SEARCH_TEXT',
+  searchText: 'work'
+});
+
+console.log('searchText should be "work"', store.getState());
